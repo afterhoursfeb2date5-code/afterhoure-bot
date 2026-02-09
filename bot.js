1e69de1185e4538c1b6ea127ee4ed900e2e0f4c3
afterhoursfeb2date5-code <afterhoursfeb2date5@gmail.com>
2026-02-09 16:53:40 +0700

    fix: emoji6.1

--- bot.js
+++ bot.js
@@ -1716,7 +1716,7 @@ client.on('interactionCreate', async (interaction) => {
                 const suggestionEmbed = new EmbedBuilder()
                     .setColor(0x5865F2)
                     .setAuthor({
-                        name: "",
+                        name: "AFTER HOURS SUGGESTIONS",
                         iconURL: "https://i.imgur.com/U76N6jc.png"
                     })
                     .setTitle('New Suggestion!')
